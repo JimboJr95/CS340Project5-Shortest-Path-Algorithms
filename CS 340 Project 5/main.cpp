f31ff7857fb10f562a9e980dd48daecaeea78a60
Jimmy Callahan <jamage95@gmail.com>
2017-10-21 20:37:22 -0500

    Initial Commit

---
+++ CS 340 Project 5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS 340 Project 5
+//
+//  Created by Jimmy Callahan on 10/21/17.
+//  Copyright © 2017 Jimmy Callahan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
